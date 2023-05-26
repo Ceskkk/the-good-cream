90e70da7ace7d60c6885483204ec48df202980a6
Cesk <ceskk97@gmail.com>
2023-05-26 12:56:57 +0200

    Refactor html structure

--- src/components/menu/desktop/HamburgerMenuDesktop.tsx
+++ src/components/menu/desktop/HamburgerMenuDesktop.tsx
@@ -15,33 +15,37 @@ function HamburgerMenuDesktop (): JSX.Element {
   return (
     <>
       <HamburgerIcon onClick={() => toggleMenu(!isMenuOpen)} />
-      <nav className={`${isMenuOpen ? '' : 'left-[-400px]'} fixed z-20 left-0 top-0 w-[400px] h-full bg-white px-16 py-10 flex flex-col overflow-y-auto shadow-xl transition-all duration-700 scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-400 hover:scrollbar-thumb-tgc-green`}>
+      <section className={`${isMenuOpen ? '' : 'left-[-400px]'} fixed z-20 left-0 top-0 w-[400px] h-full bg-white px-16 py-10 flex flex-col overflow-y-auto shadow-xl transition-all duration-700 scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-400 hover:scrollbar-thumb-tgc-green`}>
         <CrossIcon onClick={() => toggleMenu(!isMenuOpen)} />
-        <h2 className={titleStyles}>Productos</h2>
-        <ul>
-          {productMenuList.map((product) => (
-            <li key={product.id}>
-              <a href={product.link} className={linkStyles}>
-                {product.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <h2 className={titleStyles}>Más información</h2>
-        <ul>
-          {moreInfoMenuList.map((item) => (
-            <li key={item.id}>
-              <a href={item.link} className={linkStyles}>
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <nav>
+          <h2 className={titleStyles}>Productos</h2>
+          <ul>
+            {productMenuList.map((product) => (
+              <li key={product.id}>
+                <a href={product.link} className={linkStyles}>
+                  {product.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <nav>
+          <h2 className={titleStyles}>Más información</h2>
+          <ul>
+            {moreInfoMenuList.map((item) => (
+              <li key={item.id}>
+                <a href={item.link} className={linkStyles}>
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
         <ul className='flex mt-auto gap-2 align-center pt-24'>
           <li><a href='#'><InstagramIcon /></a></li>
           <li><a href='#'><FacebookIcon /></a></li>
         </ul>
-      </nav>
+      </section>
       <div onClick={() => toggleMenu(!isMenuOpen)} className={`${isMenuOpen ? '' : 'opacity-0 -z-10 pointer-events-none'} fixed z-10 w-full bg-black/[.25] top-0 right-[0] h-full cursor-pointer transition-opacity duration-700`} />
     </>
   )
--- src/components/menu/mobile/HamburgerMenuMobile.tsx
+++ src/components/menu/mobile/HamburgerMenuMobile.tsx
@@ -15,14 +15,14 @@ function HamburgerMenuMobile (): JSX.Element {
   return (
     <>
       <HamburgerIcon onClick={() => toggleMenu(!isMenuOpen)} />
-      <nav className={`${isMenuOpen ? '' : 'left-[-100%]'} fixed z-20 left-0 top-0 w-full h-full bg-white flex flex-col overflow-y-auto transition-all duration-700`}>
+      <section className={`${isMenuOpen ? '' : 'left-[-100%]'} fixed z-20 left-0 top-0 w-full h-full bg-white flex flex-col overflow-y-auto transition-all duration-700`}>
         <div className={`${divStyles} !flex-row`}>
           <CrossIcon onClick={() => toggleMenu(!isMenuOpen)} />
           <h1 className='flex w-full justify-center ml-[-22px] uppercase text-2xl' style={{ fontFamily: 'Special Elite' }}>
             <a href='#'>The good cream</a>
           </h1>
         </div>
-        <div className={divStyles}>
+        <nav className={divStyles}>
           <h2 className={titleStyles}>Productos</h2>
           <ul>
             {productMenuList.map((product) => (
@@ -33,8 +33,8 @@ function HamburgerMenuMobile (): JSX.Element {
               </li>
             ))}
           </ul>
-        </div>
-        <div className={`${divStyles} !border-b-0`}>
+        </nav>
+        <nav className={`${divStyles} !border-b-0`}>
           <h2 className={titleStyles}>Más información</h2>
           <ul>
             {moreInfoMenuList.map((item) => (
@@ -45,9 +45,9 @@ function HamburgerMenuMobile (): JSX.Element {
               </li>
             ))}
           </ul>
-        </div>
+        </nav>
         <SettingsMenuMobile />
-      </nav>
+      </section>
     </>
   )
 }
